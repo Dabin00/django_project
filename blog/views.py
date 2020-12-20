6f85a63b783bf8c8b3294c07d50a1a181e22902d
Dabin00 <73816782+Dabin00@users.noreply.github.com>
2020-12-21 01:30:19 +0900

    polls/views.py

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic.edit import DeleteView
+
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -35,7 +37,15 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
+
+
     else:
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('/blog')
+
